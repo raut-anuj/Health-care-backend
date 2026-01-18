66211fd43610ca400e10551f84d65c7018eef9ca
Anuj Kumar <personeluse2000@gmail.com>
2026-01-18 23:19:03 +0530

    feat: Add staff and admin models, implement staff controller for authentication and management

---
+++ src/models/admin.model.js
@@ -0,0 +1,77 @@
+import mongoose, {Schema}  from "mongoose"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const adminSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    emailId:{
+        type:String,
+        require:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    bloodgroup:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    sex:{
+        type:String,
+        enum:["Male","Female","Other"],
+        required:true
+    },
+    contactNumber:{
+      type:Number,
+      required:true
+    },
+    experinceinYears:{
+        type:Number,
+        default:0
+    },
+    role:
+    {
+        type:[ "AdminBoy", "Nurse" ],
+        required: true
+    },
+}, {timestamps:true})
+
+adminSchema.methods.isPasswordCorrect = async function(password){
+        return await bcrypt.compare(password,this.password)
+}
+    
+adminSchema.methods.generateAccessToken = function(){
+        return jwt.sign(
+            {
+            _id:this._id,
+            email:this.email
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {  expiresIn : process.env.ACCESS_TOKEN_EXPIRY  }       
+       )
+}
+    
+adminSchema.methods.generateRefreshToken=function(){
+        return jwt.sign(
+            {
+                id : this._id
+            },
+             process.env.REFRESH_TOKEN_SECRET,
+             {  expiresIn:process.env.REFRESH_TOKEN_EXPIRY  }       
+        )
+}
+    
+adminSchema.pre("save", async function () {
+        if (!this.isModified("password")) return;
+    
+        this.password = await bcrypt.hash(this.password, 10);
+});
+
+export const Admin = mongoose.model("Admin", adminSchema) 
